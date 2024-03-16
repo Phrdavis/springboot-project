0d249a9c107fff210604ff80a5c5a721817ef0c9
Phrdavis <davipi_sou@hotmail.com>
2024-03-16 17:04:45 -0300

    New Update

---
+++ src/main/java/com/springboot/Main.java
@@ -0,0 +1,60 @@
+package com.springboot;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    
+    public static void main(String[] args){
+
+        SpringApplication.run(Main.class, args);
+
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet(){
+
+        return new GreetResponse
+        (
+            "Hello", 
+           List.of("Javascript", "Java", "Python"), 
+            new Person("Davi", 21, 0.1)
+        );
+
+    }
+
+    record Person(String name, int age, double savings){};
+    
+    record GreetResponse
+    (
+        
+        String greet,
+        List<String> favProgrammingLanguages,
+        Person person
+
+    ){
+
+            public GreetResponse
+            (
+                String greet, 
+                List<String> favProgrammingLanguages,
+                Person person
+            )
+            {
+
+                this.greet = greet; 
+                this.favProgrammingLanguages = favProgrammingLanguages; 
+                this.person = person; 
+
+            }
+
+
+        }
+
+}
